b96e2d9e46a8d151d081d668d05609a552e9fe13
surendra96 <sawantsurendra96@gmail.com>
2019-10-14 00:55:51 +0530

    displaying list of heroes

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../heto';
+import { HEROES } from '../mock-heroes';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  heroes = HEROES;
+  selectedHero: Hero;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+}
